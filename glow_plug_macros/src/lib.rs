61c8e2db5c23f3cf3959968d72a62fa19edc50e7
Wes Wickwire <wickwirew@gmail.com>
2024-06-04 18:10:59 -0400

    Clean up

--- glow_plug_macros/src/lib.rs
+++ glow_plug_macros/src/lib.rs
@@ -1,61 +1,41 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn};
+use syn::{parse_macro_input, punctuated::Punctuated, ItemFn, Signature, Type};
 
 #[proc_macro_attribute]
 pub fn test(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(item as ItemFn);
+    let func = parse_macro_input!(item as ItemFn);
 
-    let test_fn_name = &input.sig.ident;
-    let test_fn_name_inner = syn::Ident::new(
-        &format!("{}_glow_plug_inner", test_fn_name),
-        test_fn_name.span(),
-    );
-    let return_type = &input.sig.output;
-    let block = &input.block;
-    let inputs = &input.sig.inputs;
+    let conn_type = get_conn_type(&func);
+    let db_name = test_db_name(&func);
+    let (sig, sig_inner) = get_signatures(&func);
+    let inner_ident = &sig_inner.ident;
+    let inner_block = &func.block;
 
-    // The type of connection, e.g. `PgConnection` or `MysqlConnection`
-    let conn_type = match inputs.first() {
-        Some(syn::FnArg::Typed(syn::PatType { ty, .. })) => ty,
-        _ => panic!("Expected at least one input argument"),
-    };
-
-    // The temporary database name that the test will use
-    let test_db_name = format!("{}_{}", test_fn_name, rand::random::<u32>());
-
-    let is_async = input.sig.asyncness.is_some();
-
-    let async_modifier = if is_async {
-        quote! { async }
+    let run_test = if func.sig.asyncness.is_some() {
+        quote! {
+            use glow_plug::FutureExt;
+            let result = #inner_ident(conn).catch_unwind().await;
+        }
     } else {
-        quote! {}
+        quote! {
+            let result = std::panic::catch_unwind(|| {
+                #inner_ident(conn)
+            });
+        }
     };
 
-    let test_macro = if is_async {
+    let test_macro = if func.sig.asyncness.is_some() {
         quote! { #[glow_plug::tokio::test] }
     } else {
         quote! { #[test] }
     };
 
-    let run_test = if is_async {
-        quote! {
-            use glow_plug::FutureExt;
-            let result = #test_fn_name_inner(conn).catch_unwind().await;
-        }
-    } else {
-        quote! {
-            let result = std::panic::catch_unwind(|| {
-                #test_fn_name_inner(conn)
-            });
-        }
-    };
-
     quote! {
         #test_macro
-        #async_modifier fn #test_fn_name() #return_type {
-            #async_modifier fn #test_fn_name_inner(#inputs) #return_type #block
+        #sig {
+            #sig_inner #inner_block
 
             use glow_plug::{Connection, RunQueryDsl};
             use glow_plug::MigrationHarness;
@@ -67,13 +47,13 @@ pub fn test(_attr: TokenStream, item: TokenStream) -> TokenStream {
             let mut main_conn = <#conn_type>::establish(db_url.as_str())
                 .expect("Failed to establish connection.");
 
-            diesel::sql_query(format!("CREATE DATABASE {};", #test_db_name))
+            diesel::sql_query(format!("CREATE DATABASE {};", #db_name))
                 .execute(&mut main_conn)
                 .expect("Failed to create database.");
 
             // We cannot just change the `conn` to point to the new database since not
             // all databases don't support a `USE database` command. So just connect again
-            let mut conn = <#conn_type>::establish(format!("{}/{}", db_url, #test_db_name).as_str())
+            let mut conn = <#conn_type>::establish(format!("{}/{}", db_url, #db_name).as_str())
                 .expect("Failed to establish connection.");
 
             // Run the migrations, there may be a better way to do this. Right now it currently
@@ -85,7 +65,7 @@ pub fn test(_attr: TokenStream, item: TokenStream) -> TokenStream {
             // Make sure to catch the panic, so we can drop the database even on failure.
             #run_test
 
-            diesel::sql_query(format!("DROP DATABASE {};", #test_db_name))
+            diesel::sql_query(format!("DROP DATABASE {};", #db_name))
                 .execute(&mut main_conn)
                 .expect("Failed to drop database.");
 
@@ -96,4 +76,32 @@ pub fn test(_attr: TokenStream, item: TokenStream) -> TokenStream {
         }
     }
     .into()
+}
+
+fn get_signatures(func: &ItemFn) -> (Signature, Signature) {
+    let mut sig = func.sig.clone();
+    let mut sig_inner = func.sig.clone();
+
+    // Remove the inputs of the outer function
+    sig.inputs = Punctuated::new();
+
+    // Rename the inner function
+    sig_inner.ident = syn::Ident::new(&format!("{}_inner", sig.ident), sig.ident.span());
+
+    (sig, sig_inner)
+}
+
+// Gets the type of connection, e.g. `PgConnection` or `MysqlConnection`
+fn get_conn_type(func: &ItemFn) -> &Box<Type> {
+    let args = &func.sig.inputs;
+
+    match args.first() {
+        Some(syn::FnArg::Typed(syn::PatType { ty, .. })) => ty,
+        _ => panic!("Expected at least one input argument"),
+    }
+}
+
+fn test_db_name(func: &ItemFn) -> String {
+    let name = func.sig.ident.to_string();
+    format!("{}_{}", name, rand::random::<u32>())
 }
